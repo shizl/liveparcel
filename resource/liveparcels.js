d6dbc9bd1546dd8ea6098366edb4d7fb7dc2f7e4
max <max@example.com>
2014-02-20 18:16:48 +0800

    fix setting page and package page

--- resource/liveparcels.js
+++ resource/liveparcels.js
@@ -13,39 +13,38 @@ function savenote(nid,atype){
 		atype= oldposition==position && oldposition>0 ? 'update':'updateposition';
 		
 		var package_name=jQuery("#n"+nid+" .package_name").val();
-		var dead_weight=parseFloat(jQuery("#n"+nid+" .dead_weight").val());
+		var weight=parseFloat(jQuery("#n"+nid+" .weight").val());
 		
-		var dimen_weight=parseFloat(jQuery("#n"+nid+" .dimen_weight").val());
+
 		var plength=parseFloat(jQuery("#n"+nid+" .plength").val());
 		var pprice=parseFloat(jQuery("#n"+nid+" .pprice").val());
-		dead_weight=dead_weight.toFixed(3);
-		dimen_weight=dimen_weight.toFixed(3);
+		weight=weight.toFixed(3);
 		plength=plength.toFixed(3);
 		pprice=pprice.toFixed(3);
 	
-		if(position >0 && package_name !='' && dead_weight>0 && dimen_weight>0 && plength >0 && pprice >0){
+		if(position >0 && package_name !='' && weight>0  && plength >0 && pprice >0){
 		var aid=parseInt(jQuery('.packagedata .package-line:last').index())-1;
 		if( aid > 1 && position<=aid){
 			if(position>1){
-				if(dead_weight <parseFloat(jQuery(".package-line:eq("+(position-2)+") .dead_weight").val()) || dimen_weight < parseFloat(jQuery(".package-line:eq("+(position-2)+") .dimen_weight").val()) || plength < parseFloat(jQuery(".package-line:eq("+(position-2)+") .plength").val()) || ( dead_weight==parseFloat(jQuery(".package-line:eq("+(position-2)+") .dead_weight").val()) && dimen_weight==parseFloat(jQuery(".package-line:eq("+(position-2)+") .dimen_weight").val()) && plength==parseFloat(jQuery(".package-line:eq("+(position-2)+") .plength").val())) ){
+				if(weight <parseFloat(jQuery(".package-line:eq("+(position-2)+") .weight").val()) || plength < parseFloat(jQuery(".package-line:eq("+(position-2)+") .plength").val()) || weight==parseFloat(jQuery(".package-line:eq("+(position-2)+") .weight").val()) && plength==parseFloat(jQuery(".package-line:eq("+(position-2)+") .plength").val())) {
 					alert('Please input values larger than the last row or in between the row you are trying to add to.');
 					return false;
 				}
                 if(position==2 && aid==2){
              
-					if(dead_weight >parseFloat(jQuery(".package-line:eq("+(position-1)+") .dead_weight").val()) || dimen_weight > parseFloat(jQuery(".package-line:eq("+(position-1)+") .dimen_weight").val()) || plength > parseFloat(jQuery(".package-line:eq("+(position-1)+") .plength").val()) || ( dead_weight==parseFloat(jQuery(".package-line:eq("+(position-1)+") .dead_weight").val()) && dimen_weight==parseFloat(jQuery(".package-line:eq("+(position-1)+") .dimen_weight").val()) && plength==parseFloat(jQuery(".package-line:eq("+(position-1)+") .plength").val()) && position != oldposition ) ){
+					if(weight >parseFloat(jQuery(".package-line:eq("+(position-1)+") .weight").val()) || plength > parseFloat(jQuery(".package-line:eq("+(position-1)+") .plength").val()) ||  (weight==parseFloat(jQuery(".package-line:eq("+(position-1)+") .weight").val())  && plength==parseFloat(jQuery(".package-line:eq("+(position-1)+") .plength").val()) && position != oldposition ) ){
 						alert('Please input values larger than the last row or in between the row you are trying to add to.');
 						return false;
 					}
                }else{
-               	if(dead_weight >parseFloat(jQuery(".package-line:eq("+position+") .dead_weight").val()) || dimen_weight > parseFloat(jQuery(".package-line:eq("+position+") .dimen_weight").val()) || plength > parseFloat(jQuery(".package-line:eq("+position+") .plength").val()) || ( dead_weight==parseFloat(jQuery(".package-line:eq("+position+") .dead_weight").val()) && dimen_weight==parseFloat(jQuery(".package-line:eq("+position+") .dimen_weight").val()) && plength==parseFloat(jQuery(".package-line:eq("+position+") .plength").val()) && position != oldposition ) ){
+               	if(weight >parseFloat(jQuery(".package-line:eq("+position+").weight").val()) || plength > parseFloat(jQuery(".package-line:eq("+position+") .plength").val()) ||  (weight==parseFloat(jQuery(".package-line:eq("+position+") .weight").val())  && plength==parseFloat(jQuery(".package-line:eq("+position+") .plength").val()) && position != oldposition ) ){
 					alert('Please input values larger than the last row or in between the row you are trying to add to.');
 					return false;
 				}
                }
 				var newid=position;
 			}else{
-				if(dead_weight >parseFloat(jQuery("#n1 .dead_weight").val()) || dimen_weight > parseFloat(jQuery("#n1 .dimen_weight").val()) || plength > parseFloat(jQuery("#n1 .plength").val()) || ( dead_weight==parseFloat(jQuery("#n1 .dead_weight").val()) && dimen_weight==parseFloat(jQuery("#n1 .dimen_weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
+				if(weight >parseFloat(jQuery("#n1 .weight").val())  || plength > parseFloat(jQuery("#n1 .plength").val()) || (weight==parseFloat(jQuery("#n1 .weight").val())  && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
 					alert('Please input values larger than the last row or in between the row you are trying to add to.');
 					return false;
 				}
@@ -54,13 +53,13 @@ function savenote(nid,atype){
 					
 		}else if(aid>1){
 			if(position>1){
-				if(dead_weight <parseFloat(jQuery("#n"+aid+" .dead_weight").val()) || dimen_weight < parseFloat(jQuery("#n"+aid+" .dimen_weight").val()) || plength < parseFloat(jQuery("#n"+aid+" .plength").val()) || ( dead_weight==parseFloat(jQuery("#n"+aid+" .dead_weight").val()) && dimen_weight==parseFloat(jQuery("#n"+aid+" .dimen_weight").val()) && plength==parseFloat(jQuery("#n"+aid+" .plength").val()) && position != oldposition ) ){
+				if(weight <parseFloat(jQuery("#n"+aid+" .weight").val()) || plength < parseFloat(jQuery("#n"+aid+" .plength").val()) || ( weight==parseFloat(jQuery("#n"+aid+" .weight").val()) && plength==parseFloat(jQuery("#n"+aid+" .plength").val()) && position != oldposition ) ){
 						alert('Please input values larger than the last row or in between the row you are trying to add to.');
 						return false;
 				}
 				var newid=aid+1;
 			}else{
-                if(dead_weight >parseFloat(jQuery("#n1 .dead_weight").val()) || dimen_weight > parseFloat(jQuery("#n1 .dimen_weight").val()) || plength > parseFloat(jQuery("#n1 .plength").val()) || ( dead_weight==parseFloat(jQuery("#n1 .dead_weight").val()) && dimen_weight==parseFloat(jQuery("#n1 .dimen_weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
+                if(weight >parseFloat(jQuery("#n1 .weight").val()) ||  plength > parseFloat(jQuery("#n1 .plength").val()) || ( weight==parseFloat(jQuery("#n1 .weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
 					alert('Please input values larger than the last row or in between the row you are trying to add to.');
 					return false;
 				}
@@ -68,13 +67,13 @@ function savenote(nid,atype){
 			}
 		}else if(aid>0){
 			if(position>1){
-				if(dead_weight <parseFloat(jQuery("#n1 .dead_weight").val()) || dimen_weight < parseFloat(jQuery("#n1 .dimen_weight").val()) || plength < parseFloat(jQuery("#n1 .plength").val()) || ( dead_weight==parseFloat(jQuery("#n1 .dead_weight").val()) && dimen_weight==parseFloat(jQuery("#n1 .dimen_weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val())  && position != oldposition  ) ){
+				if(weight <parseFloat(jQuery("#n1 .weight").val())  || plength < parseFloat(jQuery("#n1 .plength").val()) || ( weight==parseFloat(jQuery("#n1 .weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val())  && position != oldposition  ) ){
 						alert('Please input values larger than the last row or in between the row you are trying to add to.');
 						return false;
 				}
 				var newid=2;
 			}else{
-                if(dead_weight >parseFloat(jQuery("#n1 .dead_weight").val()) || dimen_weight > parseFloat(jQuery("#n1 .dimen_weight").val()) || plength > parseFloat(jQuery("#n1 .plength").val()) || ( dead_weight==parseFloat(jQuery("#n1 .dead_weight").val()) && dimen_weight==parseFloat(jQuery("#n1 .dimen_weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
+                if(weight >parseFloat(jQuery("#n1 .weight").val())  || plength > parseFloat(jQuery("#n1 .plength").val()) || (weight==parseFloat(jQuery("#n1 .weight").val()) && plength==parseFloat(jQuery("#n1 .plength").val()) && position != oldposition ) ){
 					alert('Please input values larger than the last row or in between the row you are trying to add to.');
 					return false;
 				}
@@ -84,7 +83,7 @@ function savenote(nid,atype){
 			newid=1;
 		}
 		
-		jQuery.post('/admin/liveparcels/settings/managepackage',{position:newid,oldposition:oldposition,package_name:package_name,dead_weight:dead_weight,dimen_weight:dimen_weight,plength:plength,pprice:pprice,atype:atype},function(data){
+		jQuery.post('/admin/liveparcels/settings/managepackage',{position:newid,oldposition:oldposition,package_name:package_name,weight:weight,plength:plength,pprice:pprice,atype:atype},function(data){
 				if(data){
                
                 document.location.reload();
@@ -105,7 +104,7 @@ $(document).ready(function(){
 			var ndata="";
 			packageData=eval('(' + data + ')');	
 			for (k in packageData){
-				ndata+='<li class="package-line" id="n'+packageData[k].position+'"><div class="field-small"><input type="hidden" class="oldposition small-text" value="'+packageData[k].position+'" ><input type="text" class="position small-text hide" value="'+packageData[k].position+'" size="4">'+packageData[k].position+'</div><div class="field"><input type="text" value="'+packageData[k].package_name+'" class="package_name small-text"></div><div class="field"><input type="text" class="dead_weight small-text" value="'+packageData[k].dead_weight+'"></div><div class="field"><input type="text" class="dimen_weight small-text" value="'+packageData[k].dimen_weight+'"></div><div class="field-small"><input type="text" class="plength small-text" value="'+packageData[k].plength+'"></div><div class="field-small"><input type="text" class="pprice small-text" value="'+packageData[k].pprice+'"></div><div class="field"><a href="####" onclick="savenote('+packageData[k].position+',\'update\')">Edit </a> / <a href="####" onclick="savenote('+packageData[k].position+',\'delete\')">Delete</a></div></li>';		
+				ndata+='<li class="package-line" id="n'+packageData[k].position+'"><div class="field-small"><input type="hidden" class="oldposition small-text" value="'+packageData[k].position+'" ><input type="text" class="position small-text hide" value="'+packageData[k].position+'" size="4">'+packageData[k].position+'</div><div class="field"><input type="text" value="'+packageData[k].package_name+'" class="package_name small-text"></div><div class="field"><input type="text" class="weight small-text" value="'+packageData[k].weight+'"></div><div class="field-small"><input type="text" class="plength small-text" value="'+packageData[k].plength+'"></div><div class="field-small"><input type="text" class="pprice small-text" value="'+packageData[k].pprice+'"></div><div class="field"><a href="####" onclick="savenote('+packageData[k].position+',\'update\')">Edit </a> / <a href="####" onclick="savenote('+packageData[k].position+',\'delete\')">Delete</a></div></li>';		
 					}
 			
 			jQuery(".packagedata li:first").after(ndata);
