c5b331cb2630204245557e14bb314a09fd592312
max <max@example.com>
2014-09-12 16:21:55 +0800

    update admin order

--- lp_admin_order/lp_admin_order.js
+++ lp_admin_order/lp_admin_order.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function(){
 
  jQuery('#checkout_reload').click(function(){
 
-       user_mail = jQuery('#admin_order_settings-pane .form-item-panes-admin-order-settings-edit-parcel-user input').val();
        discount = jQuery('#admin_order_settings-pane .form-item-panes-admin-order-settings-edit-parcel-discount input').val();
        order_id = jQuery('#admin_order_settings-pane .admin_order_id').val();
 
@@ -12,7 +11,7 @@ jQuery(document).ready(function(){
            parr.push(price);
        });
 
-   jQuery.post("/liveparcels/reload_checkout_render/ajax",{parr:parr,user_mail:user_mail,discount:discount,order_id:order_id},function(data){
+   jQuery.post("/liveparcels/reload_checkout_render/ajax",{parr:parr,discount:discount,order_id:order_id},function(data){
            window.location.reload();     
       },"json");
 
